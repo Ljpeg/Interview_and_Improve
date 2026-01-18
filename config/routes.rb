1a3f0b070c561b3740b29dcb8b2c2b644c9e1e5a
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-18 00:27:52 +0000

    syntax changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # Routes for the Job application resource:
   resources :job_applications
   #------------------------------
-
-
-  #------------------------------
-
   devise_for :users
   root to: "job_applications#index"
 end
