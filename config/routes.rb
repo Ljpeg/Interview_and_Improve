e63aa27306e15d23352d39d1794c274c5a7d34d6
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-18 06:55:09 +0000

    nested routes interviews inside of job_application

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
 Rails.application.routes.draw do
-  # Routes for the Interview resource:
-
-  # CREATE
-  post("/insert_interview", { :controller => "interviews", :action => "create" })
-
-  # READ
-  get("/interviews", { :controller => "interviews", :action => "index" })
-
-  get("/interviews/:path_id", { :controller => "interviews", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_interview/:path_id", { :controller => "interviews", :action => "update" })
-
-  # DELETE
-  get("/delete_interview/:path_id", { :controller => "interviews", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Job application resource:
-  resources :job_applications
+  resources :job_applications do
+    resources :interviews
+  end 
   #------------------------------
   devise_for :users
   root to: "job_applications#index"
