a00c52335a53138324e6a78563ee3a7bf4baf1ea
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-19 17:58:38 +0000

    updates update action, edit and show view, for edit like to update reflection

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -30,19 +30,15 @@ class ReflectionsController < ApplicationController
   end 
 
   def update
-    the_id = params.fetch("path_id")
-    the_reflection = Reflection.where({ :id => the_id }).at(0)
+    @interview = Interview.find(params.fetch(:interview_id))
+    reflection_attributes = params.require(:reflection).permit(:positive_notes, :negative_notes, :primary_gap)
+    @reflection = @interview.build_reflection(reflection_attributes)
 
-    the_reflection.interview_id = params.fetch("query_interview_id")
-    the_reflection.positive_notes = params.fetch("query_positive_notes")
-    the_reflection.negative_notes = params.fetch("query_negative_notes")
-    the_reflection.primary_gap = params.fetch("query_primary_gap")
-
-    if the_reflection.valid?
-      the_reflection.save
-      redirect_to("/reflections/#{the_reflection.id}", { :notice => "Reflection updated successfully." } )
+    if @reflection.valid?
+      @reflection.save
+      redirect_to interview_reflection_path, notice:  "Reflection updated successfully."
     else
-      redirect_to("/reflections/#{the_reflection.id}", { :alert => the_reflection.errors.full_messages.to_sentence })
+      redirect_to interview_reflection_path, alert:  @reflection.errors.full_messages.to_sentence
     end
   end
 
