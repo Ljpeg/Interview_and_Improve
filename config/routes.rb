3cd0944d2098096c5a7d55cd970b11e3e53bdb96
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-19 04:31:34 +0000

    updates to nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,13 @@
 Rails.application.routes.draw do
-  # Routes for the Reflection resource:
-
-  # CREATE
-  post("/insert_reflection", { :controller => "reflections", :action => "create" })
-
-  # READ
-  get("/reflections", { :controller => "reflections", :action => "index" })
-
-  get("/reflections/:path_id", { :controller => "reflections", :action => "show" })
-
-  # UPDATE
-
-  post("/modify_reflection/:path_id", { :controller => "reflections", :action => "update" })
-
-  # DELETE
-  get("/delete_reflection/:path_id", { :controller => "reflections", :action => "destroy" })
 
   #------------------------------
-
   resources :job_applications do
-    resources :interviews
-  end 
+    resources :interviews, shallow: true do
+      resources :reflections do
+        resources :learning_items, shallow: true
+      end
+    end
+  end
   #------------------------------
   devise_for :users
   root to: "job_applications#index"
