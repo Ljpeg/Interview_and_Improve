a8e602e758a5594fea8086cf327c2fffdd0bce33
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-19 07:31:18 +0000

    update singluar resource syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #------------------------------
   resources :job_applications do
     resources :interviews, shallow: true do
-      resource :reflections do
+      resource :reflection do
         resources :learning_items, shallow: true
       end
     end
