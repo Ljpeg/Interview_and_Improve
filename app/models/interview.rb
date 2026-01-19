d06491a3ee646673832f2f1e6a868bbc38888bc7
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-19 00:46:13 +0000

    adds associations and validation logic

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -13,7 +13,10 @@
 #
 class Interview < ApplicationRecord
   belongs_to :job_application, class_name: "JobApplication", foreign_key: "application_id", counter_cache: true
+  has_one  :reflections, class_name: "Reflection", foreign_key: "interview_id", dependent: :destroy
+
   enum :outcome, [:pending, :passed, :rejected]
   enum :interview_type, [:screening, :technical, :behavorial, :final]
+
   validates :interview_type, presence: true
 end
--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -11,4 +11,12 @@
 #  interview_id   :integer
 #
 class Reflection < ApplicationRecord
+  belongs_to :interview, required: true, class_name: "Interview", foreign_key: "interview_id"
+
+  def went_poorly_if_rejected
+    return unless interview&.rejected?
+
+    if went_poorly.blank?
+      errors.add(:went_poorly, "You must record what went poorly if an interview is rejected")
+  end 
 end
