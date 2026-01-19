fa7a456c7cdda6738e6832bd4903dc86ba40a20f
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-19 07:32:07 +0000

    updates singular resource syntax

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -13,7 +13,7 @@
 #
 class Interview < ApplicationRecord
   belongs_to :job_application, class_name: "JobApplication", foreign_key: "application_id", counter_cache: true
-  has_one  :reflections, class_name: "Reflection", foreign_key: "interview_id", dependent: :destroy
+  has_one  :reflection, class_name: "Reflection", foreign_key: "interview_id", dependent: :destroy
 
   enum :outcome, [:pending, :passed, :rejected]
   enum :interview_type, [:screening, :technical, :behavorial, :final]
