79e325e80bf04f8b411ac5686d12502b6d61c752
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2026-01-22 05:46:18 +0000

    LearningItem actions all working

--- app/controllers/learning_items_controller.rb
+++ app/controllers/learning_items_controller.rb
@@ -1,40 +1,34 @@
 class LearningItemsController < ApplicationController
+  before_action :set_learning_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_interview, only: [:new, :index, :create]
+
   def new
-    @interview = Interview.find(params.fetch(:interview_id))
-    @learning_item = @interview.reflection.learning_items.build
-  end 
+    @learning_item = @reflection.learning_items.build
+  end
 
   def index
-    @interview = Interview.find(params.fetch(:interview_id))
-    @learning_items = LearningItem.all.order({ created_at: :desc })
+    @learning_items = @reflection.learning_items
   end
 
   def show
-    @learning_item = LearningItem.find(params.fetch(:id))
-    @interview = @learning_item.reflection.interview
   end
 
   def create
-    @interview = Interview.find(params.fetch(:interview_id))
-    learning_item_attributes = params.require(:learning_item).permit(:category, :description, :status)
-    @learning_item = @interview.reflection.learning_items.build(learning_item_attributes)
+    @learning_item = @reflection.learning_items.build(learning_item_params)
 
     if @learning_item.valid?
       @learning_item.save
-      redirect_to interview_reflection_path(@learning_item.reflection.interview.id), notice: "Learning item created successfully." 
+      redirect_to interview_reflection_path(@learning_item.reflection.interview.id), notice: "Learning item created successfully."
     else
       render "learning_items/new"
     end
   end
 
   def edit
-    @learning_item = LearningItem.find(params.fetch(:id))
   end
 
   def update
-    @learning_item = LearningItem.find(params.fetch(:id))
-    learning_item_attributes = params.require(:learning_item).permit(:category, :description, :status)
-    @learning_item.assign_attributes(learning_item_attributes)
+    @learning_item.assign_attributes(learning_item_params)
 
     if @learning_item.valid?
       @learning_item.save
@@ -48,6 +42,32 @@ class LearningItemsController < ApplicationController
     @learning_item = LearningItem.find(params.fetch(:id))
     @learning_item.destroy
 
-    redirect_to interview_reflection_path(@learning_item.reflection.interview.id), notice: "Learning item deleted successfully." 
+    redirect_to interview_reflection_path(@learning_item.reflection.interview.id), notice: "Learning item deleted successfully."
+  end
+
+  private
+
+  def set_interview
+    @interview = find_scoped!(
+      Interview.within_job_applications(job_application_scope),
+      id: params.fetch(:interview_id),
+    )
+
+    @reflection = @interview.reflection
+    @job_application = @interview.job_application
+  end
+
+  def set_learning_item
+    @learning_item = find_scoped!(
+      LearningItem.within_job_applications(job_application_scope),
+      id: params.fetch(:id),
+    )
+    @reflection = @learning_item.reflection
+    @interview = @reflection.interview
+    @job_application = @interview.job_application
+  end
+
+  def learning_item_params
+    params.require(:learning_item).permit(:category, :description, :status)
   end
 end
